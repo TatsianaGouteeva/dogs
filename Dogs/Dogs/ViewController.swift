1933381a040db2c4105c67e25bb552d13892a9e6
Tatsiana Gouteeva <gouteeva@gmail.com>
2022-07-17 21:36:33 +0300

    Initial commit

---
+++ Dogs/Dogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dogs
+//
+//  Created by Tatsiana on 7/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
