9bf13242f5c0059b29f39ce42459eb9a73cc7289
Tatsiana Gouteeva <tatsiana.gouteeva>
2022-07-19 20:35:55 +0300

    Add ImageDogCollectionViewCell

---
+++ Dogs/Dogs/View/DogDetails/ImageDogCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDogCollectionViewCell.swift
+//  Dogs
+//
+//  Created by Tatsiana Gouteeva on 19.07.22.
+//
+
+import UIKit
+
+final class ImageDogCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var dogImageView: UIImageView!
+    
+    func configure(image: String) {
+        dogImageView.image = UIImage(systemName: image)
+    }
+}
