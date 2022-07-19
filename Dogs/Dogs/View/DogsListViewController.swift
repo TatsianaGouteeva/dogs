ea22afc4f4c8835d9a47fef5a094ec39061e7da5
Tatsiana Gouteeva <tatsiana.gouteeva>
2022-07-19 20:36:49 +0300

    Add Tabbar

---
+++ Dogs/Dogs/View/DogsListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DogsListViewController.swift
+//  Dogs
+//
+//  Created by Tatsiana Gouteeva on 18.07.22.
+//
+
+import UIKit
+
+final class DogsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
--- Dogs/Dogs/View/ProfileViewController.swift
+++ Dogs/Dogs/View/ProfileViewController.swift
@@ -1,19 +1,16 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  Dogs
 //
-//  Created by Tatsiana on 7/17/22.
+//  Created by Tatsiana Gouteeva on 18.07.22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
