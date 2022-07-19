ff7e1edef383cda7cbe8ff4d0249c96bc3c56a8a
Tatsiana Gouteeva <tatsiana.gouteeva>
2022-07-19 20:35:35 +0300

    Add models DogDescription & Dog

---
+++ Dogs/Dogs/Model/Dog.swift
@@ -0,0 +1,14 @@
+//
+//  Dog.swift
+//  Dogs
+//
+//  Created by Tatsiana Gouteeva on 19.07.22.
+//
+
+struct Dog {
+    let breed: String
+    let height: String
+    let weight: String
+    let description: String
+    let images: [String]
+}
---
+++ Dogs/Dogs/Model/DogDescription.swift
@@ -0,0 +1,13 @@
+//
+//  DogDescriptionModel.swift
+//  Dogs
+//
+//  Created by Tatsiana Gouteeva on 19.07.22.
+//
+
+struct DogDescription: Hashable {
+    let breed: String
+    let height: String
+    let weight: String
+    let description: String
+}
