1034edc3b0c6ee19d7b46688305a39ace4a267b2
Tatsiana Gouteeva <gouteeva@gmail.com>
2022-07-23 09:06:08 +0200

    Change Model

--- Dogs/Dogs/Model/Dog.swift
+++ Dogs/Dogs/Model/Dog.swift
@@ -7,15 +7,22 @@
 
 struct Dog {
     let breed: String
-    let height: String
-    let weight: String
-    let description: String
-    let images: [String]
+    let height: Double?
+    let weight: Double?
+    let description: String?
+    let images: [String]?
     
     var dictionary: [String : String] {
         return ["breed" : breed,
-                "height" : height,
-                "weight" : weight,
-                "description" : description]
+                "height" : height?.orEmptyString ?? "",
+                "weight" : weight?.orEmptyString ?? "",
+                "description" : description ?? ""]
+    }
+}
+
+extension Double {
+    
+    var orEmptyString: String {
+        String(self)
     }
 }
