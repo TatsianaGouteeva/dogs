f2365d7c63712721cffe172b5b65f860e9b08f85
Tatsiana Gouteeva <tatsiana.gouteeva>
2022-07-25 17:00:47 +0300

    Add RealmDataManager

---
+++ Dogs/Dogs/Service/RealmDataManager.swift
@@ -0,0 +1,42 @@
+//
+//  RealmDataManager.swift
+//  Dogs
+//
+//  Created by PC0002 on 25.07.22.
+//
+
+import RealmSwift
+
+final class RealmDataManager: DatabaseServiceProtocol {
+    
+    var isEmpty: Bool {
+        guard let count = try? Realm().objects(DogObject.self).count else { return true }
+        
+        return count == 0
+    }
+
+    func fetchData() -> [Dog] {
+        guard isEmpty else { return  fetchDataFromBase() }
+
+        let dogs = fetchDataFromJSON()
+        dogs.forEach { saveData(dog: $0) }
+        return dogs
+    }
+    
+    func fetchDataFromBase() -> [Dog] {
+        guard let realm = try? Realm() else { return [] }
+        
+        let dogObjects = realm.objects(DogObject.self)
+        return dogObjects.map { Dog(from: $0) }
+    }
+    
+    func fetchDataFromJSON() -> [Dog] {
+        JSONDataManager.fetchDataFromJSON()
+    }
+    
+    func saveData(dog: Dog) {
+        guard let realm = try? Realm() else { return }
+        
+        try? realm.write { realm.add(DogObject(from: dog)) }
+    }
+}
